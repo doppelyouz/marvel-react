0f96515e9d60520d971f7778709a3d95e4a290fa
anikisana <younker12345@gmail.com>
2022-12-19 15:02:28 +0600

    char-list and componentDidMount

--- marvel-react/src/components/randomChar/RandomChar.js
+++ marvel-react/src/components/randomChar/RandomChar.js
@@ -7,11 +7,6 @@ import Spinner from "../spinner/Spinner";
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 class RandomChar extends Component {
-  constructor(props) {
-    super(props);
-    this.updateChar();
-  }
-
   state = {
     char: {},
     loading: true,
@@ -19,6 +14,10 @@ class RandomChar extends Component {
   };
 
   marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.updateChar();
+  }
 
   onCharLoaded = (char) => {
     this.setState({ 
@@ -61,7 +60,7 @@ class RandomChar extends Component {
             Do you want to get to know him better?
           </p>
           <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main">
+          <button className="button button__main" onClick={this.updateChar}>
             <div className="inner">try it</div>
           </button>
           <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -73,9 +72,13 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
+  let styleImage = {'objectFit' : 'cover'};
+  if(thumbnail) {
+    styleImage = { 'objectFit': 'contain' }
+  }
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img src={thumbnail} alt="Random character" className="randomchar__img" style={styleImage} />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">
